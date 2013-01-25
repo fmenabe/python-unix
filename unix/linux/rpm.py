699c2559ddb02c22b4e2485d6eebccf105808c18
Francois Menabe <francois.menabe@unistra.fr>
2013-01-25 16:00:55 +0100

    Updating Rpm based object with functions for setting hostname and network configuration.

--- unix/linux/rpm.py
+++ unix/linux/rpm.py
@@ -16,14 +16,59 @@ def Rpm(host, root=''):
 
     host = unix.linux.Linux(host, root)
 
+
     class RpmHost(host.__class__):
-        def __init__(self, host=None, root=''):
-            super(RpmHost, self).__init__(self, host)
-#            host.__class__.__init__(self, root)
+        def __init__(self, root=''):
+            host.__class__.__init__(self, root)
+            self.__dict__.update(host.__dict__)
+
+
+        def set_hostname(self, hostname):
+            try:
+                self.write(
+                    '/etc/sysconfig/network',
+                    '\n'.join((
+                        'NETWORKING=yes',
+                        'NETWORKING_IPV6=no',
+                        'HOSTNAME=%s' % hostname
+                    ))
+                )
+                return [True, '', '']
+            except IOError as ioerr:
+                return [False, '', ioerr]
+
+
+        def set_network(self, interfaces):
+            network_root = '/etc/sysconfig/network-scripts'
+            self.rm(path.join(network_root, 'ifcfg-ext'))
+            for index, interface in enumerate(interfaces):
+                interface_conf = [
+                    'DEVICE=eth%s' % index,
+                    'BOOTPROTO=none',
+                    'ONBOOT=yes',
+                    'NETMASK=%s' % interface['netmask'],
+                    'IPADDR=%s' % interface['address'],
+                    'TYPE=Ethernet',
+                    'USERCTL=no',
+                    'IPV6INIT=no',
+                    'PEERDNS=yes'
+                ]
+
+                if 'gateway' in interface:
+                    interface_conf.insert(5, 'GATEWAY=%s' % interface['gateway'])
+
+                try:
+                    self.write(
+                        path.join(network_root, 'ifcfg-eth%s' % index),
+                        '\n'.join(interface_conf)
+                    )
+                except IOError as ioerr:
+                    return [False, '', ioerr]
+
+            return [True, '', '']
 
 
         def check_pkg(self, package):
-            self._connected()
             status, stdout = self.execute('rpm -qa')[:2]
             if status and stdout.find(package) != -1:
                 return True
