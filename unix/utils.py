6349a177865cb59b5c18515c0fe31dc1348721f0
Francois Menabe <francois.menabe@gmail.com>
2013-08-31 18:08:18 +0200

    Conversion methods in 'utils' module now return int or float according to 'toint' parameter.

--- unix/utils.py
+++ unix/utils.py
@@ -2,46 +2,55 @@
 
 import math
 
-def kb2mb(value):
+def kb2mb(value, toint=True):
     """Convert **value** size in Kb to Mb."""
-    return int(math.ceil(float(value) / 1024))
+    value = float(value) / 1024
+    return int(math.ceil(value)) if toint else value
 
 
-def kb2gb(value):
+def kb2gb(value, toint=True):
     """Convert **value** size in Kb to Gb."""
-    return int(math.ceil(float(value) / 1024**2))
+    value = float(value) / 1024**2
+    return int(math.ceil(value)) if toint else value
 
 
-def kb2tb(value):
+def kb2tb(value, toint=True):
     """Convert **value** size in Kb to Tb."""
-    return int(math.ceil(float(value) / 1024**3))
+    value = float(value) / 1024**3
+    return int(math.ceil(value)) if toint else value
 
 
-def mb2kb(value):
+def mb2kb(value, toint=True):
     """Convert **value** size in Mb to Kb."""
-    return int(math.ceil(float(value) * 1024))
+    value = float(value) * 1024
+    return int(math.ceil(value)) if toint else value
 
 
-def mb2gb(value):
+def mb2gb(value, toint=True):
     """Convert **value** size in Mb to Gb."""
-    return int(math.ceil(float(value) / 1024))
+    value = float(value) / 1024
+    return int(math.ceil(value)) if toint else value
 
 
-def mb2tb(value):
+def mb2tb(value, toint=True):
     """Convert **value** size in Kb to Mb."""
-    return int(math.ceil(float(value) / 1024**2))
+    value = float(value) / 1024**2
+    return int(math.ceil(value)) if toint else value
 
 
-def gb2kb(value):
+def gb2kb(value, toint=True):
     """Convert **value** size in Gb to Kb."""
-    return int(math.ceil(float(value) * 1024**2))
+    value = float(value) * 1024**2
+    return int(math.ceil(value)) if toint else value
 
 
-def gb2mb(value):
+def gb2mb(value, toint=True):
     """Convert **value** size in Gb to Mb."""
-    return int(math.ceil(float(value) * 1024))
+    value = float(value) * 1024
+    return int(math.ceil(value)) if toint else value
 
 
-def gb2tb(value):
+def gb2tb(value, toint=True):
     """Convert **value** size in Gb to Tb."""
-    return int(math.ceil(float(value) / 1024))
+    value = float(value) / 1024
+    return int(math.ceil(value)) if toint else value
