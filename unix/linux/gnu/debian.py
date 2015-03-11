d73065d0490afe3d8789339e1278cc0d449c691f
François Ménabé <francois.menabe@gmail.com>
2015-03-11 17:30:58 +0100

    unix:linux:gnu:debian: Add a property 'network' with functions for managing                        network. An unique has been created for generating the                        configuration of '/etc/network/interfaces' and                        '/etc/network/interfaces.d' files.

--- unix/linux/gnu/debian.py
+++ unix/linux/gnu/debian.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import weakref
 import unix
 import unix.linux as linux
 from .. import Linux, Chroot, LinuxError
@@ -8,6 +9,10 @@ from .. import Linux, Chroot, LinuxError
 DISTRIBS = ('Debian', 'Ubuntu')
 
 _HOSTNAMEFILE = '/etc/hostname'
+_NETFILE = '/etc/network/interfaces'
+_NETDIR = '/etc/network/interfaces.d'
+_LOINT = """auto lo
+iface lo inet loopback"""
 
 def Debian(host, force=False):
     unix.isvalid(host)
@@ -45,4 +50,57 @@ def Debian(host, force=False):
             with self.open(_HOSTNAMEFILE, 'w') as fhandler:
                 fhandler.write(value)
 
+
+        @property
+        def network(self):
+            return _Network(weakref.ref(self)())
+
+
     return DebianHost(root)
+
+
+class _Network:
+    def __init__(self, host):
+        self._host = host
+
+
+    def configure(self, interfaces):
+        distrib, version = self._host.distrib[0:2]
+        split_files = distrib == 'Ubuntu' and float(version) > 11.04
+
+        try:
+            with self._host.open(_NETFILE, 'w') as fhandler_main:
+                fhandler_main.write(_LOINT)
+
+                if split_files:
+                    fhandler_main.write('\nsource %s/*' % _NETDIR)
+                    if self._host.path.exists(_NETDIR):
+                        self._host.mkdir(_NETDIR)
+
+                for index, interface in enumerate(interfaces):
+                    name = interface.pop('name', 'eth%s' % index)
+                    inet = interface.pop('inet', 'dhcp')
+
+                    conf = ['auto %s' % name, 'iface %s inet %s' % (name, inet)]
+                    if inet == 'static':
+                        address = interface.pop('address')
+                        netmask = interface.pop('netmask')
+                        conf.extend(('    address %s' % address,
+                                     '    netmask %s' % netmask))
+                    else:
+                        for param in ('address', 'netmask', 'gateway'):
+                            interface.pop(param, None)
+                    conf.extend('    %s %s' % (attr, value)
+                                for attr, value in interface.items()
+                                if value)
+
+                    if split_files:
+                        int_file = os.path.join(_NETDIR, name)
+                        with self._host.open(int_file, 'w') as fhandler_int:
+                            fhandler_int.write('\n'.join(conf))
+                    else:
+                        conf.insert(0, '\n')
+                        fhandler_main.write('\n'.join(conf))
+        except OSError as err:
+            return [False, u'', err]
+        return [True, u'', u'']
