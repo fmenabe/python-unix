62d673e56ed2bb3a826e4551f76401e629542b8d
François Ménabé <francois.menabe@gmail.com>
2015-03-16 17:10:35 +0100

    unix:shell: Improve tables generation.

--- unix/shell.py
+++ unix/shell.py
@@ -65,63 +65,67 @@ def status(result, quit=False, start=76):
 #
 # Tables.
 #
-def colorize(colors, index, value):
-    try:
-        return ('\033[%sm%s\033[00m' % (colors[index], value)
-                if colors[index]
-                else value)
-    except IndexError:
-        return value
+colorize = lambda color, value: '\033[%s%s\033[00m' % (color, value)
 
 
-def table_border(columns):
+def table_border(columns, color=None):
     line = '+'
     for idx in range(0, len(columns)):
         size = columns[idx]
         for _ in range(0, size):
             line += '-'
         line += '+'
+    if color:
+        line = '\033[%s%s\033[00m' % (color, line)
     print(line)
 
 
-def table_line(columns_sizes, columns_values, columns_colors=[]):
-    if len(columns_sizes) != len(columns_values):
-        raise ShellError("print error: len of columns is different of len of "
-                         "columns values")
+def table_line(sizes, columns, colors=[], borders_color=None, indent=1):
+    if len(sizes) != len(columns):
+        raise ShellError('length of sizes is different from length of values')
 
-    lines = [[' ' * columns_sizes[__] for __ in range(len(columns_sizes))]]
-    # Parse columns
-    for column_index, column_value in enumerate(columns_values):
-        column_size = columns_sizes[column_index] - 2
-
-        if not isinstance(column_value, (str)):
-            column_value = str(column_value)
+    lines = []
+    for index, value in enumerate(columns):
+        size = sizes[index] - 2
+        if not isinstance(value, str):
+            value = str(value)
 
         line_number = 0
-        # Split column on new line.
-        values = column_value.split('\n')
-        for value in values:
-            # Split on column len.
-            line_value = ' %s ' % value[:column_size]
-            if len(line_value) - 1 <= column_size:
-                line_value += ' ' * (column_size - len(value))
-            value_lines = [colorize(columns_colors, column_index, line_value)]
+        column_lines = []
 
-            value = value[column_size:]
-            while value:
-                line_value = '  %s ' % value[:(column_size - 1)]
-                if len(line_value) - 2 <= column_size :
-                    line_value += ' ' * (column_size - (len(line_value) - 2))
-                value_lines.append(colorize(columns_colors,
-                                             column_index,
-                                             line_value))
-                value = value[(column_size - 1):]
+        # Split column value on new line.
+        value_lines = value.split('\n')
+        for line in value_lines:
+            # Split line on word.
+            line = line.split()
 
-            for line in value_lines:
-                if line_number > len(lines) - 1:
-                    lines.append([' ' * columns_sizes[__]
-                                  for __ in range(len(columns_sizes))])
-                lines[line_number][column_index] = line
-                line_number += 1
+            # Append words until it exceed column size.
+            cur_line = ''
+            while line:
+                word = line.pop(0)
+                if (len(cur_line) + 1 + len(word)) > size:
+                    cur_line += ' ' * (size - (len(cur_line) - 2))
+                    column_lines.append(cur_line)
+                    cur_line = ' ' * indent
+                cur_line += ' ' + word
+            cur_line += ' ' * (size - (len(cur_line) - 2))
+            column_lines.append(cur_line)
 
-    print('\n'.join('|%s|' % '|'.join(line) for line in lines))
+        # Add column lines.
+        for line in column_lines:
+            if line_number > len(lines) - 1:
+                # Initialize a new line.
+                new_line = []
+                for __ in range(len(sizes)):
+                   new_column = ' ' * sizes[__]
+                   if colors and colors[__]:
+                       new_column = colorize(colors[index], new_column)
+                   new_line.append(new_column)
+                lines.append(new_line)
+            if colors and colors[index]:
+                line = colorize(colors[index], line)
+            lines[line_number][index] = line
+            line_number += 1
+
+    border = '|' if not borders_color else colorize(borders_color, '|')
+    print('\n'.join(border + border.join(line) + border for line in lines))
